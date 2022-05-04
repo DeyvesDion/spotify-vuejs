1a4fd082ffb2e3781905d9be58f77605b381484f
DeyvesDion <deyvesdion@gmail.com>
2022-05-04 16:51:38 +0200

    Download update

--- src/components/Download/Download.vue
+++ src/components/Download/Download.vue
@@ -1,6 +1,26 @@
 <template>
     <div>
-        <p>téléchargement</p>
+        <div class="download">
+            <div class="container">
+                <div class="free">
+                    <h3>SPOTIFY FREE</h3>
+                </div>
+                <div class="title">
+                    <h1>Écouter, ça change tout</h1>
+                </div>
+                <div class="description">
+                    <h2>
+                        Des millions de titres et de podcasts. Aucune carte<br />
+                        de crédit nécessaire.
+                    </h2>
+                </div>
+                <div class="btnArrea">
+                    <button type="button" class="btn">
+                        Télécharher Spotify free
+                    </button>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
