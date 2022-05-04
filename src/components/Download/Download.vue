1c82d1d65c6a7e5312de062440cc1a7e83f09910
DeyvesDion <deyvesdion@gmail.com>
2022-05-04 16:17:47 +0200

    Download init

---
+++ src/components/Download/Download.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p>téléchargement</p>
+    </div>
+</template>
+
+<script>
+export default {
+    setup() {
+        return {};
+    },
+};
+</script>
+
+<style src="./Download.css" scoped></style>
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,16 +1,21 @@
 <script>
 import Hero from "..//..//components/Hero/Hero.vue";
+import Download from "..//..//components/Download/Download.vue";
 export default {
     setup() {
         return {};
     },
     components: {
         Hero,
+        Download,
     },
 };
 </script>
 <template>
-    <div><Hero /></div>
+    <div>
+        <Hero />
+        <Download />
+    </div>
 </template>
 
 <style lang="css" scoped></style>
